a615a4f22edfbcfbd248005245c820e7a7743684
Morten S. Laursen <morten@softwarehuset.dk>
2013-03-10 15:59:23 +0100

    Added Omnirota module

---
+++ fmLib/platform_support/sdu_omnirota/src/wheelModule.cpp
@@ -0,0 +1,190 @@
+/****************************************************************************
+# FroboMind wheelModule.cpp
+# Copyright (c) 2011-2013, author Morten S. Laursen <morten@softwarehuset.dk>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#	* Redistributions of source code must retain the above copyright
+#  	notice, this list of conditions and the following disclaimer.
+#	* Redistributions in binary form must reproduce the above copyright
+#  	notice, this list of conditions and the following disclaimer in the
+#  	documentation and/or other materials provided with the distribution.
+#	* Neither the name FroboMind nor the
+#  	names of its contributors may be used to endorse or promote products
+#  	derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+****************************************************************************/
+#include "wheelModule.hpp"
+#include "OmnirotaCMDValues.hpp"
+
+wheelModule::wheelModule()
+:local_node_handler("~"),global_node_handler()
+{
+//	angle_vel = 0;
+//	drive_vel = 0;
+//	vel_to_motor_const = 0;
+
+	active = false;
+	deadman = false;
+
+	last_deadman_received = ros::Time::now();
+};
+
+void wheelModule::makeItSpin( void )
+{
+	local_node_handler.param<std::string>(	"cmd_vel_drive_sub", 	topics.cmd_vel_drive,	"/fmActuators/drive_velocity"	);
+	local_node_handler.param<std::string>(	"cmd_pos_angle_sub", 	topics.cmd_pos_angle,	"/fmActuators/steering_angle"	);
+	local_node_handler.param<std::string>(	"enc_pos_angle_pub", 	topics.encoder_angle,	"/fmInformation/wheel_angle"	);
+	local_node_handler.param<std::string>(	"can_tx_cmd_pub", 		topics.can_tx_cmd,		"/fmCSP/can0_tx"				);
+	local_node_handler.param<std::string>(	"raw_encoder", 			topics.raw_encoder,		"/fmSensors/raw_encoder"		);
+	local_node_handler.param<std::string>(	"raw_encoder_edge",		topics.encoder_edge,	"/fmSensors/raw_encoder_edge"	);
+
+	local_node_handler.param<double>(		"wheel_diameter", 		parameters.wheel_diameter,	0.7		);
+	local_node_handler.param<double>(		"ticks_pr_round", 		parameters.ticks_pr_round,	42		);
+
+	local_node_handler.param<double>(		"angle_regulator_interval",	parameters.interval,		1	);
+
+	/* m/s -> ticks/entry * 1 m/s = 1/(pi*wheel_diameter) rps = ticks_pr_round/(pi*wheel_diameter) ticks/sec = */
+//	vel_to_motor_const = ( parameters.ticks_pr_round / ( 3.14 * parameters.wheel_diameter ) ) * ( 1 / parameters.ms_in_between );
+
+	subscribers.cmd_vel_drive = global_node_handler.subscribe<geometry_msgs::TwistStamped>(topics.cmd_vel_drive, 2,&wheelModule::on_cmd_vel_drive,this);
+	subscribers.cmd_pos_angle = global_node_handler.subscribe<msgs::steering_angle_cmd>(topics.cmd_pos_angle,2,&wheelModule::on_cmd_pos_angle,this);
+	subscribers.raw_encoder	  = global_node_handler.subscribe<msgs::encoder>(topics.raw_encoder,2,&wheelModule::on_raw_encoder,this);
+	subscribers.encoder_edge  = global_node_handler.subscribe<msgs::encoder>(topics.encoder_edge,2,&wheelModule::on_encoder_edge,this);
+//	subscribers.deadman = 		global_node_handler.subscribe<std_msgs::Bool>(topics.deadman,2,&FrobitInterface::on_deadman,this);
+//	subscribers.nmea = 			global_node_handler.subscribe<msgs::nmea>(topics.nmea_sub,2,&FrobitInterface::on_encoder,this);
+
+//	publishers.nmea = 			global_node_handler.advertise<msgs::nmea>(topics.nmea_pub, 1);
+//	publishers.encoder_drive_vel = 	global_node_handler.advertise<geometry_msgs::TwistStamped>(topics.encoder_drive_vel, 1);
+	publishers.encoder_angle_pos = 	global_node_handler.advertise<msgs::encoder>(topics.encoder_angle, 1);
+	publishers.can_tx_cmd		 = 	global_node_handler.advertise<msgs::can>(topics.can_tx_cmd, 1);
+
+	ros::Timer t1= global_node_handler.createTimer(ros::Duration(parameters.interval),&wheelModule::on_timer,this);
+
+	messages.pwr_on_msg.id = OMNIROTA_DRIVING_MOTOR_SEND;
+	messages.pwr_on_msg.flags = 0;
+	messages.pwr_on_msg.data[0] = OMNIROTA_CMD_POWER_ON;
+	messages.pwr_on_msg.length = 1;
+
+	ros::spin();
+}
+
+void wheelModule::on_cmd_vel_drive(const geometry_msgs::TwistStamped::ConstPtr& msg)
+{
+	messages.cmd_vel_drive = *msg;
+	drive_vel_sp = messages.cmd_vel_drive.twist.linear.x * 100;
+	int velocity = drive_vel_sp*10; //rpm
+	this->setVelocity(velocity);
+}
+
+void wheelModule::on_cmd_pos_angle(const msgs::steering_angle_cmd::ConstPtr& msg)
+{
+	messages.cmd_pos_angle = *msg;
+}
+
+void wheelModule::on_raw_encoder(const msgs::encoder::ConstPtr& msg)
+{
+	messages.raw_encoder = *msg;
+}
+
+void wheelModule::on_encoder_edge(const msgs::encoder::ConstPtr& msg)
+{
+	const int encoder_edge_min_valid = 512; //Defines the angular range where 
+											//a edge on the inductive detector
+											//is valid
+	const int encoder_edge_max_valid = 768;
+	messages.raw_encoder_edge = *msg;
+	int encoder_offset_min_valid = (1024-angle_encoder_offset + encoder_edge_min_valid)%1024;
+	int encoder_offset_max_valid = (1024-angle_encoder_offset + encoder_edge_max_valid)%1024;
+	int curr_encoder_edge = messages.raw_encoder_edge.encoderticks;
+	if((curr_encoder_edge >= encoder_offset_min_valid) && 
+		(curr_encoder_edge <= encoder_offset_max_valid))
+		angle_encoder_offset = curr_encoder_edge;
+}
+
+void wheelModule::on_deadman(const std_msgs::Bool::ConstPtr& msg)
+{
+	deadman = msg->data;
+	last_deadman_received = ros::Time::now();
+}
+
+//void wheelModule::on_encoder(const msgs::nmea::ConstPtr& msg)
+//{
+//	messages.encoder.header.stamp = msg->header.stamp;
+//	messages.encoder.encoderticks = boost::lexical_cast<int>(msg->data.at(1));
+//	publishers.encoder_left.publish(messages.encoder);
+
+//	messages.encoder.encoderticks = boost::lexical_cast<int>(msg->data.at(2));
+//	publishers.encoder_right.publish(messages.encoder);
+//}
+
+void wheelModule::on_timer(const ros::TimerEvent& e)
+{
+		const double angle_to_enc = 1024.0f/360.f;
+		const double P = 1;
+		
+		publishers.can_tx_cmd.publish(messages.pwr_on_msg);
+
+		angle_pos_sp = messages.cmd_pos_angle.steering_angle;
+		double sp = angle_pos_sp * angle_to_enc;
+		angle_encoder_pos = (int)(1024-angle_encoder_offset + messages.raw_encoder.encoderticks)%1024;
+		double err = sp - angle_encoder_pos;
+		double correction = err * P;
+		if(correction < 0)
+			correction *=-1;
+		if(correction>512)
+			correction = 512;
+		double out;
+		if(sp>angle_encoder_pos)
+			out = (int)(1024+angle_encoder_pos + correction)%1024;
+		else
+			out = (int)(1024+angle_encoder_pos - correction)%1024;
+	//build can message
+	int out_set = out+0.5f;
+	this->setAngularVelocity(out_set);
+}
+
+void wheelModule::setVelocity(int velocity)
+{
+	messages.vel_can_msg.header.stamp = ros::Time::now();
+	messages.vel_can_msg.id = OMNIROTA_DRIVING_MOTOR_SEND;
+	messages.vel_can_msg.flags = 0;
+	messages.vel_can_msg.data[0] = OMNIROTA_CMD_SET_SPEED;
+	if(velocity>0)
+	 messages.vel_can_msg.data[1] = OMNIROTA_MOTOR_DIRECTION_FORWARD;
+	else
+	{
+	 messages.vel_can_msg.data[1] = OMNIROTA_MOTOR_DIRECTION_BACKWARD;
+	 velocity *= -1;
+	}	
+	messages.vel_can_msg.data[2] = velocity & 0xff;
+	messages.vel_can_msg.data[3] = (velocity>>8) & 0xff;
+	messages.vel_can_msg.length = 4;
+	//publish message
+	publishers.can_tx_cmd.publish(messages.vel_can_msg);
+}
+
+void wheelModule::setAngularVelocity(int velocity)
+{
+	messages.angle_vel_can_msg.header.stamp = ros::Time::now();
+	messages.angle_vel_can_msg.id = OMNIROTA_STEERING_MOTOR_SEND;
+	messages.angle_vel_can_msg.flags = 0;
+	messages.angle_vel_can_msg.data[0] = OMNIROTA_CMD_TURN;
+	messages.angle_vel_can_msg.data[1] = velocity & 0xff;
+    messages.angle_vel_can_msg.data[2] = (velocity>>8) & 0xff;
+    messages.angle_vel_can_msg.length = 3;
+	//publish message
+	publishers.can_tx_cmd.publish(messages.angle_vel_can_msg);
+}
+
---
+++ fmLib/platform_support/sdu_omnirota/src/wheelModule.hpp
@@ -0,0 +1,178 @@
+/****************************************************************************
+# FroboMind wheelModule.hpp
+# Copyright (c) 2011-2013, author Morten S. Laursen <morten@softwarehuset.dk>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#	* Redistributions of source code must retain the above copyright
+#  	notice, this list of conditions and the following disclaimer.
+#	* Redistributions in binary form must reproduce the above copyright
+#  	notice, this list of conditions and the following disclaimer in the
+#  	documentation and/or other materials provided with the distribution.
+#	* Neither the name FroboMind nor the
+#  	names of its contributors may be used to endorse or promote products
+#  	derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+****************************************************************************/
+#ifndef WHEELMODULE_HPP_
+#define WHEELMODULE_HPP_
+
+#include <ros/ros.h>
+#include <string.h>
+#include <sstream>
+#include <boost/lexical_cast.hpp>
+#include <msgs/can.h>
+#include <msgs/steering_angle_cmd.h>
+#include <msgs/encoder.h>
+#include <std_msgs/Bool.h>
+#include <geometry_msgs/TwistStamped.h>
+
+/**
+ * \mainpage
+ *
+ * \ref Node
+ *
+ * \ref Params
+ *
+ * \ref Topics
+ *
+ *
+ *
+ */
+
+/**
+ *
+ * @addtogroup Node
+ * @{
+ * Class for controlling the OmniRota
+ *
+ * This class controls the wheels of the OmniRota given a commanded linear velocity and
+ * a steering wheel angle.
+ */
+class wheelModule
+{
+private:
+	double 				angle_pos_sp, drive_vel_sp;
+	int angle_encoder_offset, angle_encoder_pos;
+
+	bool 				active;
+	bool				deadman;
+
+	ros::Time 			last_deadman_received;
+
+	void setVelocity(int velocity);
+	void setAngularVelocity(int velocity);
+
+	struct
+	{
+		/** @addtogroup Node
+		 * @{
+		 * @addtogroup Params
+		 * @{
+		 */
+		double 				wheel_diameter;			//!< diameter of the wheels. [m]
+		double				ticks_pr_round;			//!< conversion factor. [ticks/round]
+//		double				ms_in_between;			//!< period time of the control loop [ms]
+		double				interval;				//!< period of the timer publishing motor commands [ms]
+//		double 				max_velocity;			//!< maximum possible motor command
+//		double				timeout;				//!< watchdog timeout [s]
+		/**@} @}*/
+	} parameters;
+
+	struct
+	{
+		/** @addtogroup Node
+		 * @{
+		 * @addtogroup Params
+		 * @{
+		 */
+		geometry_msgs::TwistStamped 	cmd_vel_drive;
+		msgs::steering_angle_cmd 	cmd_pos_angle;
+//		msgs::nmea 					motor_command;
+		msgs::encoder 				raw_encoder, raw_encoder_edge;
+		msgs::can					vel_can_msg, angle_vel_can_msg, pwr_on_msg;
+		/** @} */
+	} messages;
+
+	struct
+	{
+		/** @addtogroup Topics
+		 * @{
+		 *
+		 * Node topics
+		 */
+		std::string 		cmd_vel_drive;	//!< the commanded velocity input from higher layers @type \ref TwistStamped
+		std::string			cmd_pos_angle;	//!< the commanded velocity input from higher layers @type \ref steering_angle_cmd
+		std::string 		can_tx_cmd;	//!< the commanded can message output to lower layers @type \ref can
+		std::string			encoder_angle;	//!< the encoder output to higher layers @type \ref encoder
+		std::string			raw_encoder;	//!< the encoder input from lower layers @type \ref encoder
+		std::string			encoder_edge;	//!< the encoder input from lower layers @type \ref encoder
+		std::string			deadman;		//!< the deadman input from higher layers @type \ref StringStamped
+//		std::string			nmea_sub;		//!< the nmea input from lower layers @type \ref nmea
+//		std::string			encoder_drive;	//!< the encoder input from lower layers @type \ref encoder
+//		std::string			nmea_pub;		//!< the nmea output to lower layers @type \ref nmea
+		/** @} */
+	} topics;
+
+	struct
+	{
+		/** @addtogroup Node
+		 * @{
+		 * @addtogroup Params
+		 * @{
+		 */
+		ros::Subscriber 	cmd_vel_drive;
+		ros::Subscriber		cmd_pos_angle;
+		ros::Subscriber		raw_encoder;
+		ros::Subscriber		encoder_edge;
+		ros::Subscriber		deadman;
+//		ros::Subscriber		nmea;
+		/** @} */
+	} subscribers;
+
+
+
+public:
+	ros::NodeHandle 	global_node_handler;
+	ros::NodeHandle		local_node_handler;
+
+	struct
+	{
+		/** @addtogroup Node
+		 * @{
+		 * @addtogroup Params
+		 * @{
+		 */
+//		ros::Publisher 		nmea;
+//		ros::Publisher		encoder_drive_vel;
+		ros::Publisher		encoder_angle_pos;
+		ros::Publisher		can_tx_cmd;
+		/** @} */
+	} publishers;
+
+
+
+	wheelModule();
+
+	void makeItSpin(void);
+	void on_cmd_vel_drive(const geometry_msgs::TwistStamped::ConstPtr&);
+	void on_cmd_pos_angle(const msgs::steering_angle_cmd::ConstPtr&);
+	void on_raw_encoder(const msgs::encoder::ConstPtr&);
+	void on_encoder_edge(const msgs::encoder::ConstPtr&);
+	void on_deadman(const std_msgs::Bool::ConstPtr&);
+//	void on_encoder(const msgs::nmea::ConstPtr&);
+	void on_timer(const ros::TimerEvent&);
+};
+/** @}*/
+#endif /*WHEELMODULE_HPP_*/
